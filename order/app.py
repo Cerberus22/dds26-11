c9a27fe53e996a18ddbf6061c3b77bb8eb7d1fd0
aecelen <a.e.celen@student.tudelft.nl>
2026-03-03 13:11:32 +0100

    Switch to async Flask with Quart

--- order/app.py
+++ order/app.py
@@ -1,15 +1,15 @@
 import logging
 import os
-import atexit
 import random
 import uuid
 from collections import defaultdict
 
-import redis
-import requests
+import aiohttp
+from redis.asyncio import Redis
+from redis.exceptions import RedisError
 
 from msgspec import msgpack, Struct
-from flask import Flask, jsonify, abort, Response
+from quart import Quart, jsonify, abort, Response
 
 
 DB_ERROR_STR = "DB error"
@@ -17,19 +17,26 @@ REQ_ERROR_STR = "Requests error"
 
 GATEWAY_URL = os.environ['GATEWAY_URL']
 
-app = Flask("order-service")
+app = Quart("order-service")
 
-db: redis.Redis = redis.Redis(host=os.environ['REDIS_HOST'],
-                              port=int(os.environ['REDIS_PORT']),
-                              password=os.environ['REDIS_PASSWORD'],
-                              db=int(os.environ['REDIS_DB']))
+db: Redis = Redis(host=os.environ['REDIS_HOST'],
+                  port=int(os.environ['REDIS_PORT']),
+                  password=os.environ['REDIS_PASSWORD'],
+                  db=int(os.environ['REDIS_DB']))
 
-
-def close_db_connection():
-    db.close()
+session: aiohttp.ClientSession | None = None
 
 
-atexit.register(close_db_connection)
+@app.before_serving
+async def startup():
+    global session
+    session = aiohttp.ClientSession()
+
+
+@app.after_serving
+async def shutdown():
+    await session.close()
+    await db.aclose()
 
 
 class OrderValue(Struct):
@@ -39,11 +46,11 @@ class OrderValue(Struct):
     total_cost: int
 
 
-def get_order_from_db(order_id: str) -> OrderValue | None:
+async def get_order_from_db(order_id: str) -> OrderValue | None:
     try:
         # get serialized data
-        entry: bytes = db.get(order_id)
-    except redis.exceptions.RedisError:
+        entry: bytes = await db.get(order_id)
+    except RedisError:
         return abort(400, DB_ERROR_STR)
     # deserialize data if it exists else return null
     entry: OrderValue | None = msgpack.decode(entry, type=OrderValue) if entry else None
@@ -54,18 +61,18 @@ def get_order_from_db(order_id: str) -> OrderValue | None:
 
 
 @app.post('/create/<user_id>')
-def create_order(user_id: str):
+async def create_order(user_id: str):
     key = str(uuid.uuid4())
     value = msgpack.encode(OrderValue(paid=False, items=[], user_id=user_id, total_cost=0))
     try:
-        db.set(key, value)
-    except redis.exceptions.RedisError:
+        await db.set(key, value)
+    except RedisError:
         return abort(400, DB_ERROR_STR)
     return jsonify({'order_id': key})
 
 
 @app.post('/batch_init/<n>/<n_items>/<n_users>/<item_price>')
-def batch_init_users(n: int, n_items: int, n_users: int, item_price: int):
+async def batch_init_users(n: int, n_items: int, n_users: int, item_price: int):
 
     n = int(n)
     n_items = int(n_items)
@@ -85,15 +92,15 @@ def batch_init_users(n: int, n_items: int, n_users: int, item_price: int):
     kv_pairs: dict[str, bytes] = {f"{i}": msgpack.encode(generate_entry())
                                   for i in range(n)}
     try:
-        db.mset(kv_pairs)
-    except redis.exceptions.RedisError:
+        await db.mset(kv_pairs)
+    except RedisError:
         return abort(400, DB_ERROR_STR)
     return jsonify({"msg": "Batch init for orders successful"})
 
 
 @app.get('/find/<order_id>')
-def find_order(order_id: str):
-    order_entry: OrderValue = get_order_from_db(order_id)
+async def find_order(order_id: str):
+    order_entry: OrderValue = await get_order_from_db(order_id)
     return jsonify(
         {
             "order_id": order_id,
@@ -105,51 +112,51 @@ def find_order(order_id: str):
     )
 
 
-def send_post_request(url: str):
+async def send_post_request(url: str):
     try:
-        response = requests.post(url)
-    except requests.exceptions.RequestException:
+        response = await session.post(url)
+    except aiohttp.ClientError:
         abort(400, REQ_ERROR_STR)
     else:
         return response
 
 
-def send_get_request(url: str):
+async def send_get_request(url: str):
     try:
-        response = requests.get(url)
-    except requests.exceptions.RequestException:
+        response = await session.get(url)
+    except aiohttp.ClientError:
         abort(400, REQ_ERROR_STR)
     else:
         return response
 
 
 @app.post('/addItem/<order_id>/<item_id>/<quantity>')
-def add_item(order_id: str, item_id: str, quantity: int):
-    order_entry: OrderValue = get_order_from_db(order_id)
-    item_reply = send_get_request(f"{GATEWAY_URL}/stock/find/{item_id}")
-    if item_reply.status_code != 200:
+async def add_item(order_id: str, item_id: str, quantity: int):
+    order_entry: OrderValue = await get_order_from_db(order_id)
+    item_reply = await send_get_request(f"{GATEWAY_URL}/stock/find/{item_id}")
+    if item_reply.status != 200:
         # Request failed because item does not exist
         abort(400, f"Item: {item_id} does not exist!")
-    item_json: dict = item_reply.json()
+    item_json: dict = await item_reply.json()
     order_entry.items.append((item_id, int(quantity)))
     order_entry.total_cost += int(quantity) * item_json["price"]
     try:
-        db.set(order_id, msgpack.encode(order_entry))
-    except redis.exceptions.RedisError:
+        await db.set(order_id, msgpack.encode(order_entry))
+    except RedisError:
         return abort(400, DB_ERROR_STR)
     return Response(f"Item: {item_id} added to: {order_id} price updated to: {order_entry.total_cost}",
                     status=200)
 
 
-def rollback_stock(removed_items: list[tuple[str, int]]):
+async def rollback_stock(removed_items: list[tuple[str, int]]):
     for item_id, quantity in removed_items:
-        send_post_request(f"{GATEWAY_URL}/stock/add/{item_id}/{quantity}")
+        await send_post_request(f"{GATEWAY_URL}/stock/add/{item_id}/{quantity}")
 
 
 @app.post('/checkout/<order_id>')
-def checkout(order_id: str):
+async def checkout(order_id: str):
     app.logger.debug(f"Checking out {order_id}")
-    order_entry: OrderValue = get_order_from_db(order_id)
+    order_entry: OrderValue = await get_order_from_db(order_id)
     # get the quantity per item
     items_quantities: dict[str, int] = defaultdict(int)
     for item_id, quantity in order_entry.items:
@@ -158,21 +165,21 @@ def checkout(order_id: str):
     # for rollback purposes.
     removed_items: list[tuple[str, int]] = []
     for item_id, quantity in items_quantities.items():
-        stock_reply = send_post_request(f"{GATEWAY_URL}/stock/subtract/{item_id}/{quantity}")
-        if stock_reply.status_code != 200:
+        stock_reply = await send_post_request(f"{GATEWAY_URL}/stock/subtract/{item_id}/{quantity}")
+        if stock_reply.status != 200:
             # If one item does not have enough stock we need to rollback
-            rollback_stock(removed_items)
+            await rollback_stock(removed_items)
             abort(400, f'Out of stock on item_id: {item_id}')
         removed_items.append((item_id, quantity))
-    user_reply = send_post_request(f"{GATEWAY_URL}/payment/pay/{order_entry.user_id}/{order_entry.total_cost}")
-    if user_reply.status_code != 200:
+    user_reply = await send_post_request(f"{GATEWAY_URL}/payment/pay/{order_entry.user_id}/{order_entry.total_cost}")
+    if user_reply.status != 200:
         # If the user does not have enough credit we need to rollback all the item stock subtractions
-        rollback_stock(removed_items)
+        await rollback_stock(removed_items)
         abort(400, "User out of credit")
     order_entry.paid = True
     try:
-        db.set(order_id, msgpack.encode(order_entry))
-    except redis.exceptions.RedisError:
+        await db.set(order_id, msgpack.encode(order_entry))
+    except RedisError:
         return abort(400, DB_ERROR_STR)
     app.logger.debug("Checkout successful")
     return Response("Checkout successful", status=200)
@@ -181,6 +188,4 @@ def checkout(order_id: str):
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
 else:
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
+    logging.basicConfig(level=logging.INFO)
